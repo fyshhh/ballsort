1aa71cdb4d280bfb8d21acc60c65601fde214936
Illio Suardi <>
2020-09-11 22:26:16 +0800

    Abstract sort method

--- BallSort.java
+++ BallSort.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -267,17 +270,71 @@ public class BallSort {
         }
     }
 
+    public static void sort(int num, State state) {
+
+        StopWatch sw = new StopWatch();
+        sw.start();
+
+        var steps = new PriorityQueue<Pair<State, List<Pair<Integer, Integer>>>>(
+                (x, y) -> (y.fst.completeTubes() - x.fst.completeTubes()));
+        var allStates = new HashSet<State>();
+        List<Pair<Integer, Integer>> sol = null;
+        steps.add(new Pair<>(state, new ArrayList<>()));
+        while (!steps.isEmpty()) {
+            var move = steps.poll();
+            var currState = move.fst;
+            var prevSteps = move.snd;
+            if (currState.isComplete()) {
+                sol = prevSteps;
+                break;
+            }
+            for (int i = 0; i < num; i++) {
+                for (int j = 0; j < num; j++) {
+                    if (i != j && currState.canMove(i, j)) {
+                        var newState = currState.move(i, j);
+                        if (!allStates.contains(newState)) {
+                            var nextSteps = new ArrayList<>(prevSteps);
+                            nextSteps.add(new Pair<>(i, j));
+                            allStates.add(newState);
+                            steps.add(new Pair<>(newState, nextSteps));
+                        }
+                    }
+                }
+            }
+        }
+        if (sol == null) {
+            System.out.println("No solution detected; likely due to input error.");
+        } else {
+            System.out.println();
+            int index = 0;
+            for (Pair<Integer, Integer> p : sol) {
+                System.out.printf("%02d: Move from tube %d to %d\n", ++index, p.fst + 1, p.snd + 1);
+                if (index % 5 == 0) {
+                    System.out.println();
+                }
+            }
+            System.out.printf("\nShortest solution requires %d moves.", sol.size());
+        }
+
+        sw.stop();
+        System.out.printf("\nIterated through %d states.\n", allStates.size());
+        System.out.printf("Algorithm took %f seconds to run.\n", sw.getTime());
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        label:
         while (true) {
             System.out.print("Enter a command (use help to view commands):");
             String command = sc.nextLine();
-            if (command.equals("exit")) {
-                break;
-            } else if (command.equals("help")) {
+            switch (command) {
+            case "exit":
+                break label;
+            case "help":
                 System.out.println("\nUse \"exit\" to exit.");
                 System.out.println("Use \"sort\" to sort.");
+                System.out.println("Use load to load from a file.");
                 System.out.println("\nWhen entering into the solver, use the following format, from bottom to top: ");
                 System.out.println("\"[CLR1]<space>[CLR2]<space>[CLR3]<space>[CLR4]\"");
                 System.out.println("For instance, a tube with four blue balls should be:");
@@ -285,28 +342,39 @@ public class BallSort {
                 System.out.println("\nHere are the codes for colors: ");
                 System.out.println(
                         "\"org\" - orange,\n" +
-                        "\"red\" - red,\n" +
-                        "\"blu\" - blue,\n" +
-                        "\"pnk\" - pink,\n" +
-                        "\"lgn\" - light green,\n" +
-                        "\"gry\" - grey,\n" +
-                        "\"lbl\" - light blue,\n" +
-                        "\"olv\" - olive,\n" +
-                        "\"prp\" - purple\n");
+                                "\"red\" - red,\n" +
+                                "\"blu\" - blue,\n" +
+                                "\"pnk\" - pink,\n" +
+                                "\"lgn\" - light green,\n" +
+                                "\"gry\" - grey,\n" +
+                                "\"lbl\" - light blue,\n" +
+                                "\"olv\" - olive,\n" +
+                                "\"prp\" - purple\n");
                 System.out.println("Use \"code\" if you want to see the codes only.\n");
-            } else if (command.equals("code")) {
+                break;
+            case "code":
                 System.out.println("\nHere are the codes for colors: ");
                 System.out.println(
                         "\"org\" - orange,\n" +
-                        "\"red\" - red,\n" +
-                        "\"blu\" - blue,\n" +
-                        "\"pnk\" - pink,\n" +
-                        "\"lgn\" - light green,\n" +
-                        "\"gry\" - grey,\n" +
-                        "\"lbl\" - light blue,\n" +
-                        "\"olv\" - olive,\n" +
-                        "\"prp\" - purple\n");
-            } else if (command.equals("sort")) {
+                                "\"red\" - red,\n" +
+                                "\"blu\" - blue,\n" +
+                                "\"pnk\" - pink,\n" +
+                                "\"lgn\" - light green,\n" +
+                                "\"gry\" - grey,\n" +
+                                "\"lbl\" - light blue,\n" +
+                                "\"olv\" - olive,\n" +
+                                "\"prp\" - purple\n");
+                break;
+            case "load":
+                System.out.print("\nEnter file path here: ");
+                String filePath = sc.nextLine();
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(filePath));
+                } catch (FileNotFoundException e) {
+                    System.out.println("No file detected.");
+                }
+                break;
+            case "sort":
                 System.out.print("\nEnter the number of tubes: ");
                 int num = sc.nextInt();
                 sc.nextLine();
@@ -332,60 +400,11 @@ public class BallSort {
                         }
                     }
                 }
-
-                StopWatch sw = new StopWatch();
-                sw.start();
-
-                var steps = new PriorityQueue<Pair<State, List<Pair<Integer, Integer>>>>(
-                        (x, y) -> (y.fst.completeTubes() - x.fst.completeTubes()));
-                var allStates = new HashSet<State>();
-                List<Pair<Integer, Integer>> sol = null;
-                steps.add(new Pair<>(state, new ArrayList<>()));
-                while (!steps.isEmpty()) {
-                    var move = steps.poll();
-                    var currState = move.fst;
-                    var prevSteps = move.snd;
-                    if (currState.isComplete()) {
-                        sol = prevSteps;
-                        break;
-                    }
-                    for (int i = 0; i < num; i++) {
-                        for (int j = 0; j < num; j++) {
-                            if (i == j) {
-                            } else if (currState.canMove(i, j)) {
-                                var newState = currState.move(i, j);
-                                if (allStates.contains(newState)) {
-                                } else {
-                                    var nextSteps = new ArrayList<>(prevSteps);
-                                    nextSteps.add(new Pair<>(i, j));
-                                    allStates.add(newState);
-                                    steps.add(new Pair<>(newState, nextSteps));
-                                }
-                            }
-                        }
-                    }
-                }
-                if (sol == null) {
-                    System.out.println("No solution detected; likely due to input error.");
-                } else {
-                    System.out.println();
-                    int index = 0;
-                    for (Pair<Integer, Integer> p : sol) {
-                        System.out.printf("%02d: Move from tube %d to %d\n", ++index, p.fst + 1, p.snd + 1);
-                        if (index % 5 == 0) {
-                            System.out.println();
-                        }
-                    }
-                    System.out.printf("\nShortest solution requires %d moves.", sol.size());
-                }
-
-                sw.stop();
-                System.out.printf("\nIterated through %d states.\n", allStates.size());
-                System.out.printf("Algorithm took %f seconds to run.\n", sw.getTime());
-            } else {
-                System.out.println("Command invalid.");
+                sort(num, state);
+                break;
+            default:
+                System.out.println("\nInvalid command detected; try again.\n");
             }
         }
     }
-
 }
